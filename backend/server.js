28b86ada069e7ba399b6f10d3d95dd7f5825b171
kaarolszweda <karolsnikers@gmail.com>
2024-10-21 14:54:15 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'your_password',
+//     database: 'clinic'
+// });
+
+// app.listen(5000, () => {
+//     console.log('Server is running on port 5000');
+// });
