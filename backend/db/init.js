3e3929d1021a2d6d40c8c26b311d5379ea12fa09
kaarolszweda <karolsnikers@gmail.com>
2024-12-09 11:30:39 +0100

    CHnage password in db

--- backend/db/init.js
+++ backend/db/init.js
@@ -4,7 +4,7 @@ async function initDb() {
   const connection = await mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "KarolSzweda13",
+    password: "password",
     multipleStatements: true,
   });
 
